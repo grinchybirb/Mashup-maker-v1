3487416a1bfda6e6afae8a50cefd424a2ad8a424
rslashmemes <pgmabv99@gmail.com>
2021-05-16 17:15:23 -0400

    added stats via class and a visual for the process

--- run.py
+++ run.py
@@ -3,6 +3,13 @@ import shutil
 import zipfile
 import webbrowser
 
+
+class stats:
+    def __init__(self):
+        self.nfiles=0
+        self.nroots=0
+        self.nsuperroots=0
+
 # build html file, and show list of pngs
 def show_png(list_roots2,list_of_lists_of_files):
 
@@ -54,17 +61,20 @@ def show_png2(list_roots2,list_of_lists_of_files):
         iroot +=1
 
 
-def copy_util(source_dir,target_dir):
+def copy_util(source_dir,target_dir,mystats):
     # copying files
     i=0
     list_files=[]
     list_roots2=[]
     list_of_lists_of_files=[]
+    
     for root, dirs, files in os.walk(source_dir, topdown=True):
         print("=======",root,dirs,files)
         if len(files)!=0:
             list_roots2.append(root)
             list_of_lists_of_files.append(files)
+            mystats.nfiles +=len(files)
+            mystats.nroots +=1
 
         # print(root)
         for file in files:
@@ -74,6 +84,7 @@ def copy_util(source_dir,target_dir):
                 # print(i,full_file)
                 i +=1
 
+    mystats.nsuperroots +=1
     show_png2(list_roots2,list_of_lists_of_files)
     # it is inclusive, meaning 2 and 5 will be in it
     print("To see the images of the files type: show. To continue to selection, press ENTER.")
@@ -119,73 +130,82 @@ def copy_util(source_dir,target_dir):
 
 
 
-
-print("starting")
-sample_dir=os.path.join(".","samples")
-sword_dir=os.path.join(sample_dir,"swords")
-gui_dir=os.path.join(sample_dir,"gui")
-wool_dir=os.path.join(sample_dir,"wool")
-particle_dir=os.path.join(sample_dir,"particles")
-gapple_dir=os.path.join(sample_dir,"gapples")
-tools_dir=os.path.join(sample_dir,"tools")
-target_base =  os.path.join(".","target") 
-zipfile_name=os.path.join(".","walrus")
-
-
-# create fresh target
-if os.path.exists(target_base):
-    shutil.rmtree(target_base, ignore_errors=False, onerror=None)
-os.mkdir(target_base,0o777)
-
-# create levels needed for overlay to work
-target_dir_temp=os.path.join(target_base,"assets")
-os.mkdir(target_dir_temp,0o777)
-target_dir_temp=os.path.join(target_dir_temp,"minecraft")
-os.mkdir(target_dir_temp,0o777)
-target_dir_temp=os.path.join(target_dir_temp,"textures")
-os.mkdir(target_dir_temp,0o777)
+def main():
+    print("starting")
+    mystats=stats()
+    sample_dir=os.path.join(".","samples")
+    sword_dir=os.path.join(sample_dir,"swords")
+    gui_dir=os.path.join(sample_dir,"gui")
+    wool_dir=os.path.join(sample_dir,"wool")
+    particle_dir=os.path.join(sample_dir,"particles")
+    gapple_dir=os.path.join(sample_dir,"gapples")
+    tools_dir=os.path.join(sample_dir,"tools")
+    target_base =  os.path.join(".","target") 
+    zipfile_name=os.path.join(".","walrus")
 
 
-target_dir=os.path.join(target_dir_temp,"items")
-os.mkdir(target_dir)
+    # create fresh target
+    if os.path.exists(target_base):
+        shutil.rmtree(target_base, ignore_errors=False, onerror=None)
+    os.mkdir(target_base,0o777)
 
-# add swords
-copy_util(sword_dir,target_dir)
-# add gapples
-copy_util(gapple_dir,target_dir)
-# add tools
-# add gapples
-copy_util(tools_dir,target_dir)
-
-
-
-target_dir=os.path.join(target_dir_temp,"blocks")
-os.mkdir(target_dir)
-# add wool
-copy_util(wool_dir,target_dir)
-
-target_dir=os.path.join(target_dir_temp,"particle")
-os.mkdir(target_dir)
-# add particles
-copy_util(particle_dir,target_dir)
-
-target_dir=os.path.join(target_dir_temp,"gui")
-os.mkdir(target_dir)
-# add crosshair
-copy_util(gui_dir,target_dir)
+    # create levels needed for overlay to work
+    target_dir_temp=os.path.join(target_base,"assets")
+    os.mkdir(target_dir_temp,0o777)
+    target_dir_temp=os.path.join(target_dir_temp,"minecraft")
+    os.mkdir(target_dir_temp,0o777)
+    target_dir_temp=os.path.join(target_dir_temp,"textures")
+    os.mkdir(target_dir_temp,0o777)
 
 
+    target_dir=os.path.join(target_dir_temp,"items")
+    os.mkdir(target_dir)
+    # add swords
+    copy_util(sword_dir,target_dir,mystats)
+    # add gapples
+    copy_util(gapple_dir,target_dir,mystats)
+    # add tools
+    # add gapples
+    copy_util(tools_dir,target_dir,mystats)
+
+
+
+    target_dir=os.path.join(target_dir_temp,"blocks")
+    os.mkdir(target_dir)
+    # add wool
+    copy_util(wool_dir,target_dir,mystats)
+
+    target_dir=os.path.join(target_dir_temp,"particle")
+    os.mkdir(target_dir)
+    # add particles
+    copy_util(particle_dir,target_dir,mystats)
+
+    target_dir=os.path.join(target_dir_temp,"gui")
+    os.mkdir(target_dir)
+    # add crosshair
+    copy_util(gui_dir,target_dir,mystats)
 
 
 
 
-# add description
-full_file=os.path.join(sample_dir,"pack.mcmeta")
-shutil.copy(full_file,target_base)
 
-# add pack image
-full_file2=os.path.join(sample_dir,"pack.png")
-shutil.copy(full_file2,target_base)
-# create a zip file
-shutil.make_archive(zipfile_name, 'zip', root_dir=target_base)
-print("finished")
+
+    # add description
+    full_file=os.path.join(sample_dir,"pack.mcmeta")
+    shutil.copy(full_file,target_base)
+
+    # add pack image
+    full_file2=os.path.join(sample_dir,"pack.png")
+    shutil.copy(full_file2,target_base)
+    # create a zip file
+    shutil.make_archive(zipfile_name, 'zip', root_dir=target_base)
+    print("finished","superroots",mystats.nsuperroots,"roots",mystats.nroots,"files",mystats.nfiles)
+
+
+
+# main()
+from pycallgraph import PyCallGraph
+from pycallgraph.output import GraphvizOutput
+
+with PyCallGraph(output=GraphvizOutput()):
+    main()
